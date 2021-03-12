f268cc053c912d3c84a68d38533cb4b3a69b41e3
Joshua Pare <65102852+sbhrule15@users.noreply.github.com>
2021-03-12 11:41:42 -0500

    Second commit.

---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Welcome to Unit 3! We will be using GitHub from now on to deal with version control.
+    }
+}

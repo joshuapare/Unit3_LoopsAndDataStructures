821c2fdde7e036bbbda109185ae80ce162a0a918
Joshua Pare <65102852+sbhrule15@users.noreply.github.com>
2021-03-12 20:01:10 -0500

    Added For Loop explanation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,37 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        // Welcome to Unit 3! We will be using GitHub from now on to deal with version control.
+        // Lets look at some common data structures:
 
-        System.out.println("Adding a short description in a print");
+        //-----For Loops------//
 
-        System.out.println("Silly goose");
+        // Lets say that I want to execute something multiple times, like print hello 5 times. I could do this:
 
-        System.out.println("Trix are for kids");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
 
-        System.out.println("Its finger-lickin' good");
+        // But that's pretty messy. Let's use a loop to make this cleaner and more logical:
+
+        for (int i = 0; i < 10; i++){
+            System.out.println("Hello");
+        }
+
+        // The syntax of the for loop is this:
+        // for (
+        // #-declare an integer and starting value to count from-# -> 'int i = 0';
+        // #-condition that tells the loop when to keep executing-# -> 'i < 10';
+        // #-how to change the counting integer every time the loop executes-# -> 'i++'
+        // ) {
+        //     do something
+        // }
+
     }
 }
